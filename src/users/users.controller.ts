da6af716c692afa743ed10cd62782e13ce655960
Elin Corovic <ec024@hdm-stuttgart.de>
2023-09-08 03:34:07 +0200

    added decorator for getting user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,14 +3,16 @@ import { Request } from 'express';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
