49dae0355f0ccb6e970d65cbfa28ae571159805c
Elin Corovic <ec024@hdm-stuttgart.de>
2023-09-24 17:59:30 +0200

    feat: GET /users/images/:filename

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
+  Header,
+  Param,
   Patch,
   Req,
+  StreamableFile,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -17,17 +21,20 @@ import { User } from '@prisma/client';
 import { UpdateUserDto } from './dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
+import { createReadStream, existsSync } from 'fs';
 
-@UseGuards(JwtGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
   }
 
+  @UseGuards(JwtGuard)
   @Patch('me')
   @UseInterceptors(FileInterceptor('img'))
   updateUser(
@@ -38,11 +45,22 @@ export class UsersController {
     return this.usersService.updateUser(userId, dto, img);
   }
 
+  @UseGuards(JwtGuard)
   @Patch('change-password')
   changePassword(
     @GetUser('id') userId: number,
     @Body() dto: ChangePasswordDto,
   ) {
     return this.usersService.changePassword(userId, dto);
+  }
+
+  @Get('images/:filename')
+  @Header('Content-Type', 'image/jpeg')
+  getImage(@Param('filename') filename: string): StreamableFile {
+    const filePath = join(process.cwd(), '/uploads/users-imgs/', filename);
+    if (!existsSync(filePath))
+      throw new BadRequestException('The requested file does not exist');
+    const file = createReadStream(filePath);
+    return new StreamableFile(file);
   }
 }
