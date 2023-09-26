d9acf8f5a520ba3012615647c5f64f4b1b9eea70
Elin Corovic <ec024@hdm-stuttgart.de>
2023-09-26 17:54:09 +0200

    feat: DELETE /ratings/:slug

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,19 +1,34 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { RatingsService } from './ratings.service';
 import { JwtGuard } from 'src/auth/guard';
 import { CreateRatingDto } from './dto';
 import { GetUser } from 'src/utils/decorators';
 
+@UseGuards(JwtGuard)
 @Controller('ratings')
 export class RatingsController {
   constructor(private readonly ratingsService: RatingsService) {}
 
-  @UseGuards(JwtGuard)
   @Post()
   createRating(
     @Body() dto: CreateRatingDto,
     @GetUser('username') username: string,
   ) {
     return this.ratingsService.createRating(dto, username);
+  }
+
+  @Delete(':slug')
+  deleteRating(
+    @Param('slug') slug: string,
+    @GetUser('username') username: string,
+  ) {
+    return this.ratingsService.deleteRating(slug, username);
   }
 }
--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateRatingDto } from './dto';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
@@ -23,6 +27,34 @@ export class RatingsService {
           if (error.meta.field_name === 'Rating_bookSlug_fkey (index)') {
             throw new BadRequestException('No book with this slug');
           }
+        }
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('You have already rated this book');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async deleteRating(slug: string, username: string) {
+    try {
+      const rating = await this.prisma.rating.delete({
+        where: {
+          username_bookSlug: {
+            username: username,
+            bookSlug: slug,
+          },
+        },
+      });
+
+      return rating;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2001') {
+          throw new BadRequestException('Rating does not exist');
+        }
+        if (error.code === 'P2025') {
+          throw new BadRequestException('Rating does not exist');
         }
       }
       throw error;
